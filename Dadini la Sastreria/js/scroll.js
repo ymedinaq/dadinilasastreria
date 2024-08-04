6106f3a5f9d2583153eab43b92d720471b40ad73
Yesenia Medina <78103823+ymedinaq@users.noreply.github.com>
2024-08-04 14:28:12 -0500

    Delete Dadini la Sastreria directory

--- Dadini la Sastreria/js/scroll.js
+++
@@ -1,8 +0,0 @@
-function moverContenedores(direccion){
-    var contenedor = document.getElementById("content-carrusel");
-    if(direccion == 'Izq'){
-        contenedor.scrollLeft-=1000;
-    }else{
-        contenedor.scrollLeft+=1000;
-    }
-}
